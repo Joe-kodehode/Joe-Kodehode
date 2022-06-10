081045937f7d37dc61aee64aae7b2944543644cc
Joe Glendenning <joe@joe.joe>
2022-06-10 09:28:19 +0200

    Created react assignment 1

--- JS Practice/Mastermind/main.js
+++ JS Practice/Mastermind/main.js
@@ -1,17 +1,12 @@
-let answer = ["red", "green", "yellow", "pink"];
+let answer = ["red", "red", "yellow", "pink"];
 
-let guess = ["red", "red", "red", "red"];
+let guess = ["yellow", "yellow", "red", "red"];
 
 let whiteCount = 0;
 
 let redCount = 0;
 
 let notIncluded = 0;
-
-// function getAnswer(arr, value) {
-//   let eAmmount = guess.forEach((v) => {
-//     v === value && eAmmount++;
-//   });
 
 guess.forEach((e, i) => {
   if (e === answer[i]) {
@@ -32,3 +27,13 @@ console.log(redCount);
 console.log(whiteCount);
 
 console.log(notIncluded);
+
+
+
+
+
+
+// function getAnswer(arr, value) {
+//   let eAmmount = guess.forEach((v) => {
+//     v === value && eAmmount++;
+//   });

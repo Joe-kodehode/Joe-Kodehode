48262e4679ee2691643cc8346e8e72cbe2bc5383
Joe Glendenning <joe.kodehode@gmail.com>
2022-09-09 09:15:45 +0200

    word-spin challenge from codewars

--- JS Practice/Task from Endre/Double-swap/main.js
+++ JS Practice/Task from Endre/Double-swap/main.js
@@ -28,7 +28,7 @@ const doubleSwap = (str, a, b) => {
 };
 
 // doubleSwap("hello how are you");
-console.log(doubleSwap("this is a string", "i", "s"));
+// console.log(doubleSwap("this is a string", "i", "s"));
 // console.log(doubleSwap("m#ybe #nother #ppro#ch is necess#ry", "#", "a"));
 // console.log(doubleSwap("what is the point of this?", "o", "t"));
 // console.log(doubleSwap("CghJh%!&hu78UHb(gb!hdsl#dakgf!dal!", "g", "!"));
@@ -41,3 +41,24 @@ console.log(doubleSwap("this is a string", "i", "s"));
 //  whao is ohe ptino tf ohis?
 //  C!hJh%g&hu78UHb(!bghdsl#dak!fgdalg
 //  elt's do onl east tlst
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+const spinWords = (string) =>
+  string
+    .split(" ")
+    .map((word) => (word.length > 4 ? word.split("").reverse().join("") : word))
+    .join(" ");
+// console.log(spinWords("Hey wollef sroirraw"));
+
+const isPangram = (string) => {
+  let alphabet = [];
+  for (let i = 97; i < 123; i++) alphabet.push(String.fromCharCode(i));
+
+  for (const char of [...string])
+    alphabet = alphabet.filter((c) => c !== char.toLowerCase());
+
+  return alphabet.length === 0;
+};
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog."));

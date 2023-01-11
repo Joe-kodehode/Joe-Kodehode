c4f1e959a80e3dc1eea1063b0a0d51904f10089c
Joe Glendenning <joe.kodehode@gmail.com>
2023-01-11 08:54:37 +0100

    pig latin codewars updated

--- JS Practice/codewars/Simple_Pig_Latin/script.js
+++ JS Practice/codewars/Simple_Pig_Latin/script.js
@@ -1,13 +1,4 @@
-function pigIt(str) {
-  let answer = [];
-  str.split(" ").map((word, i) => {
-    if (word !== "!") {
-      answer.push(word.substr(1) + word.substr(0, 1) + "ay");
-    } else {
-      answer.push(word);
-    }
-  });
-  return answer.join(" ");
-}
+const pigIt = (str) =>
+  str.split` `.map((w) => (w !== "!" ? `${w.substr(1)}${w[0]}ay` : w)).join` `;
 
 console.log(pigIt("Pig latin is cool !"));
